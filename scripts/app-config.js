d197c32a791afb7ef4ead66d8d6b77fc84f7d1b5
seanstraus22 <sean.straus@macmillan.com>
2019-04-05 15:59:02 -0500

    add paths to config

---
+++ scripts/app-config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var routeConfig;
+
+synapseWebApp.config( ['$routeProvider'] function ($routeProvider){
+	for (var paths in window.routes) {
+		var pathList = paths.split(";");
+		angular.forEach(pathList, function (path) {
+			routeConfig = window.routes[paths];
+			$routeProvider.when( path, routeConfig);
+		});
+	}
+	$routeProvider.otherwise({redirectTo: '/dashboard'});
+})
